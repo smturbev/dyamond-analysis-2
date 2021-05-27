91f16d96f9d91240c1935f742220eaf64e697583
Sami Turbeville <smturbev@uw.edu>
2021-05-26 17:12:13 -0700

    minor adjustment

--- python_scripts/w_alb-olr_jhist.py
+++ python_scripts/w_alb-olr_jhist.py
@@ -8,6 +8,7 @@ from utility import load, util
 models = ["ICON","SAM","NICAM"] # need to figure out fix for FV3's mismatched grid points
 regions = ["TWP","SHL","NAU"]
 
+# %%
 for r in regions:
     for m in models:
         # load olr, alb, w, z
@@ -46,10 +47,27 @@ for r in regions:
                 alb_binned = np.where(w[:,ind14km]<=w_thres[1], alb, np.nan)
 
             print(w_string[i], np.nanmean(olr), np.nanmean(alb))
+            if (m=="NICAM") or (m=="SAM"):
+                time_array = w.time
+            elif m=="ICON":
+                time_array = w.t
+            else:
+                raise Exception("model not defined",m)
+            if r=="TWP":
+                time_mask = (time_array.dt.hour.values>=20)
+            elif r=="NAU":
+                time_mask = (time_array.dt.hour.values>=22)or(time_array.dt.hour.values<=2)
+            elif r=="SHL":
+                time_mask = (time_array.dt.hour.values>=11)or(time_array.dt.hour.values<=15)
+            else:
+                raise Exception("region not defined,", r)
+            olr_binned = olr_binned[time_mask]
+            alb_binned = alb_binned[time_mask]
 
             fig, ax = plt.subplots(1,1,figsize=(6.5,6))
             util.dennisplot("density", olr_binned.flatten(), alb_binned.flatten(), 
                         ax=ax, model="Native "+m, region=r+"\n"+w_string[i])
-            plt.savefig("../plots/w_alb-olr_hist_{}_{}_{}.png".format(m,r,w_string[i].lower()),
+            plt.savefig("../plots/w_alb-olr_hist_{}_{}_{}.png".format(m,r,
+                        w_string[i].lower().replace(" ","_")),
                         bbox_inches="tight",pad_inches=0.5)
             plt.show()
